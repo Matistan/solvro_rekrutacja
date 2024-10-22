076f5f16b60b5046c1116b54749a5d1fb5861aeb
Mateusz Stankiewicz <matistan05@gmail.com>
2024-10-22 21:33:50 +0200

    added important message

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,7 @@ server.listen(5050, () => {
     console.log('Server is running on http://localhost:5050/');
 })
 
+// Can be public, because to connect to the database I need to add your IP address to the whitelist
 const MONGO_URI = 'mongodb+srv://matistan05:koralowa@cluster0.9t3mz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.Promise = Promise;
